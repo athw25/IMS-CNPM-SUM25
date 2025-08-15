640349a5b165ae044ee6e2b18418235d3f924e10
Ngyendaoanhthu <thunda1662@ut.edu.vn>
2025-08-15 22:04:06 +0700

    Add files via upload

---
+++ training_models.py
@@ -0,0 +1,24 @@
+# training_models.py
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from db_base import Base
+
+class TrainingProgram(Base):
+    __tablename__ = "training_programs"
+
+    progID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    goal: Mapped[str | None] = mapped_column(String(255))
+
+    # Quan hệ 1-n với Project
+    projects: Mapped[list["Project"]] = relationship(back_populates="program")
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    projID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    progID: Mapped[int] = mapped_column(ForeignKey("training_programs.progID"), nullable=False)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    # Quan hệ n-1 với TrainingProgram
+    program: Mapped["TrainingProgram"] = relationship(back_populates="projects")
