746ab62b9d1f5b59165ce08b35f364faff233074
HoVanManh <manhvan250@gmail.com>
2025-08-23 08:45:15 +0700

    Delete recruitment.py

--- recruitment.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-from recruitment_controller import router as recruitment_router
-
-app = FastAPI()
-app.include_router(recruitment_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
